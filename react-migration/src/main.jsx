5a72ff16a5699635352f855aca57a3d44078a010
dusklight00 <r.rahul.developer@gmail.com>
2025-02-07 17:54:41 +0530

    contact page migrated to react

--- react-migration/src/main.jsx
+++ react-migration/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Info from "./Info.jsx";
+import Contact from "./Contact.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Info />
+    <Contact />
   </React.StrictMode>
 );
